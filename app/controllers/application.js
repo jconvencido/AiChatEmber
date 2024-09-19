b8084b4f7cd5809c1e28b11ae9278de46541d624
jconvencido <jconvencido@fullscale.io>
2024-09-19 15:09:04 +0800

    Fix lint failing test

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,5 @@
-import Ember from 'ember';
+import Controller from '@ember/controller';
 
-export default Ember.Controller.extend({});
+export default Controller.extend({
+  // controller code here
+});
